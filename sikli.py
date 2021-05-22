f1f619bab20dd45bed24599739d36a55a2d93f1e
Myrza11 <bapaevmyrza038@gmail.com>
2021-05-22 14:05:47 +0600

    first commit

---
+++ sikli.py
@@ -0,0 +1,90 @@
+'''lang1 = 'Rust'
+languages = ['go', 'java', 'php', 'python', 'javascript', 'ruby']
+for i in languages:
+    if lang1 == i:
+        print('this languages is in list')
+
+
+
+
+
+
+w = ['go', 'java', 'php', 'python', 'javascript', 'ruby']
+d = "php"
+i=0
+while w[i] != d:
+    print(w[i])
+    i+=1
+
+
+text = 'qwerty'
+print(text[1])
+
+
+
+
+a = 7
+i=0
+while i < 5:
+    a = a * 7
+    print(a)
+    i+=1
+        
+
+
+
+
+
+
+
+
+
+
+languages = ['go', 'java', 'php', 'python', 'javascript', 'ruby']
+for i in range(0,6):
+    print(i, languages[i])
+
+
+
+'''
+
+
+i = -10
+for i in range(i,10):
+    if i < 10: 
+        print(i)
+        i+=1
+
+
+'''
+
+
+
+names = ('Максат','Лязат','Данияр','Айбек','Атай','Салават','Адинай','Жоомарт','Алымбек','Эрмек','Дастан','Бекмамат','Аслан',)
+i = 2
+while i < 12:
+    print(names[i])
+    i+=2
+
+
+
+
+
+
+
+
+names = ('Максат','Лязат','Данияр','Айбек','Атай','Салават','Адинай','Жоомарт','Алымбек','Эрмек','Дастан','Бекмамат','Аслан',)
+i = 1
+while i < 12:
+    print(names[i])
+    i+=2
+
+
+
+
+
+
+
+
+
+'''
